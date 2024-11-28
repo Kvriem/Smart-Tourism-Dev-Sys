1581cf6c5545561aa334fc2f733b2dd134d66840
Kariem <kariemg05@gmial.com>
2024-11-28 21:46:58 +0200

    Final result of scraping script and promo of dataset

--- Data_collecting/ScrapingScript.py
+++ Data_collecting/ScrapingScript.py
@@ -7,8 +7,26 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.firefox import GeckoDriverManager 
+from requests.exceptions import RequestException
 import random
 from time import sleep
+
+total_requests = 0
+selenium_requests = 0
+
+# List of proxies
+#global proxies_list
+#with open (r"C:\Users\Kariem\Desktop\Capstone_Porject\repo\Smart-Tourism-Dev-Sys\Data_collecting\valid_proxies.txt", "r", encoding="utf-8") as file:
+#    proxies_list = file.readlines()
+
+# Select a random proxy
+#proxy = random.choice(proxies_list)
+#
+## Proxies dictionary
+#proxies = {
+#    "http": f"http://{proxy}",
+#    "https": f"http://{proxy}",
+#}
 
 # Dynamic headers
 HEADERS_LIST = [
@@ -17,18 +35,42 @@ HEADERS_LIST = [
     {"User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:88.0) Gecko/20100101 Firefox/88.0"}
 ]
 
-cities=['Hurghada','Sharm El Sheikh','Ain Sokhna', 'Alexandria', 'Cairo']
-Cities_url=['https://www.booking.com/searchresults.html?ss=Hurghada%2C+Egypt&ssne=Alexandria&ssne_untouched=Alexandria&highlighted_hotels=788831&efdco=1&label=gen173nr-1FCAEoggI46AdIM1gEaEOIAQGYATG4ARfIAQ_YAQHoAQH4AQKIAgGoAgO4AtzRkroGwAIB0gIkNzI2MWQ0OTEtNzExNS00MTA4LTk0N2MtYjkwMDZmMmZlYWU32AIF4AIB&aid=304142&lang=en-us&sb=1&src_elem=sb&src=hotel&dest_id=-290029&dest_type=city&group_adults=2&no_rooms=1&group_children=0','https://www.booking.com/searchresults.html?ss=Hurghada%2C+Egypt&ssne=Alexandria&ssne_untouched=Alexandria&highlighted_hotels=788831&efdco=1&label=gen173nr-1FCAEoggI46AdIM1gEaEOIAQGYATG4ARfIAQ_YAQHoAQH4AQKIAgGoAgO4AtzRkroGwAIB0gIkNzI2MWQ0OTEtNzExNS00MTA4LTk0N2MtYjkwMDZmMmZlYWU32AIF4AIB&aid=304142&lang=en-us&sb=1&src_elem=sb&src=hotel&dest_id=-290029&dest_type=city&group_adults=2&no_rooms=1&group_children=0','https://www.booking.com/searchresults.html?ss=Sharm+El+Sheikh%2C+Egypt&ssne=Cairo&ssne_untouched=Cairo&highlighted_hotels=788831&efdco=1&label=gen173nr-1FCAEoggI46AdIM1gEaEOIAQGYATG4ARfIAQ_YAQHoAQH4AQKIAgGoAgO4AtzRkroGwAIB0gIkNzI2MWQ0OTEtNzExNS00MTA4LTk0N2MtYjkwMDZmMmZlYWU32AIF4AIB&aid=304142&lang=en-us&sb=1&src_elem=sb&src=searchresults&dest_id=-302053&dest_type=city&group_adults=2&no_rooms=1&group_children=0','https://www.booking.com/searchresults.html?ss=Alexandria%2C+Egypt&ssne=Ain+Sokhna&ssne_untouched=Ain+Sokhna&highlighted_hotels=788831&efdco=1&label=gen173nr-1FCAEoggI46AdIM1gEaEOIAQGYATG4ARfIAQ_YAQHoAQH4AQKIAgGoAgO4AtzRkroGwAIB0gIkNzI2MWQ0OTEtNzExNS00MTA4LTk0N2MtYjkwMDZmMmZlYWU32AIF4AIB&aid=304142&lang=en-us&sb=1&src_elem=sb&src=searchresults&dest_id=-290263&dest_type=city&group_adults=2&no_rooms=1&group_children=0','https://www.booking.com/searchresults.html?ss=Cairo%2C+Egypt&ssne=Alexandria&ssne_untouched=Alexandria&highlighted_hotels=788831&efdco=1&label=gen173nr-1FCAEoggI46AdIM1gEaEOIAQGYATG4ARfIAQ_YAQHoAQH4AQKIAgGoAgO4AtzRkroGwAIB0gIkNzI2MWQ0OTEtNzExNS00MTA4LTk0N2MtYjkwMDZmMmZlYWU32AIF4AIB&aid=304142&lang=en-us&sb=1&src_elem=sb&src=searchresults&dest_id=-290692&dest_type=city&group_adults=2&no_rooms=1&group_children=0']
-hotel_test=['https://www.booking.com/hotel/eg/the-grand-resort-hurghada.html?label=gen173nr-1FCAEoggI46AdIM1gEaEOIAQGYATG4ARfIAQ_YAQHoAQH4AQKIAgGoAgO4AtzRkroGwAIB0gIkNzI2MWQ0OTEtNzExNS00MTA4LTk0N2MtYjkwMDZmMmZlYWU32AIF4AIB&aid=304142&ucfs=1&arphpl=1&checkin=2024-11-26&checkout=2024-11-29&dest_id=-290029&dest_type=city&group_adults=2&req_adults=2&no_rooms=1&group_children=0&req_children=0&hpos=1&hapos=1&sr_order=popularity&srpvid=229b7882e20809ff&srepoch=1732556497&all_sr_blocks=71603801_376883182_2_85_0&highlighted_blocks=71603801_376883182_2_85_0&matching_block_id=71603801_376883182_2_85_0&sr_pri_blocks=71603801_376883182_2_85_0__21583&from_sustainable_property_sr=1&from=searchresults#tab-reviews','https://www.booking.com/hotel/eg/shellghada-blue-beach.html?label=gen173nr-1FCAEoggI46AdIM1gEaEOIAQGYATG4ARfIAQ_YAQHoAQH4AQKIAgGoAgO4AtzRkroGwAIB0gIkNzI2MWQ0OTEtNzExNS00MTA4LTk0N2MtYjkwMDZmMmZlYWU32AIF4AIB&aid=304142&ucfs=1&arphpl=1&dest_id=-290029&dest_type=city&group_adults=2&req_adults=2&no_rooms=1&group_children=0&req_children=0&hpos=2&hapos=2&sr_order=popularity&srpvid=6f6c7b5019bd0087&srepoch=1732555938&from=searchresults']
+cities=[ 'Alexandria','Sharm El Sheikh','Cairo','Ain Sokhna','Hurghada']
+Cities_url=[
+    'https://www.booking.com/searchresults.html?ss=Alexandria%2C+Egypt&ssne=Ain+Sokhna&ssne_untouched=Ain+Sokhna&highlighted_hotels=788831&efdco=1&label=gen173nr-1FCAEoggI46AdIM1gEaEOIAQGYATG4ARfIAQ_YAQHoAQH4AQKIAgGoAgO4AtzRkroGwAIB0gIkNzI2MWQ0OTEtNzExNS00MTA4LTk0N2MtYjkwMDZmMmZlYWU32AIF4AIB&aid=304142&lang=en-us&sb=1&src_elem=sb&src=searchresults&dest_id=-290263&dest_type=city&group_adults=2&no_rooms=1&group_children=0',
+    'https://www.booking.com/searchresults.html?ss=Sharm+El+Sheikh%2C+Egypt&ssne=Cairo&ssne_untouched=Cairo&highlighted_hotels=788831&efdco=1&label=gen173nr-1FCAEoggI46AdIM1gEaEOIAQGYATG4ARfIAQ_YAQHoAQH4AQKIAgGoAgO4AtzRkroGwAIB0gIkNzI2MWQ0OTEtNzExNS00MTA4LTk0N2MtYjkwMDZmMmZlYWU32AIF4AIB&aid=304142&lang=en-us&sb=1&src_elem=sb&src=searchresults&dest_id=-302053&dest_type=city&group_adults=2&no_rooms=1&group_children=0',
+    'https://www.booking.com/searchresults.html?ss=Cairo%2C+Egypt&ssne=Alexandria&ssne_untouched=Alexandria&highlighted_hotels=788831&efdco=1&label=gen173nr-1FCAEoggI46AdIM1gEaEOIAQGYATG4ARfIAQ_YAQHoAQH4AQKIAgGoAgO4AtzRkroGwAIB0gIkNzI2MWQ0OTEtNzExNS00MTA4LTk0N2MtYjkwMDZmMmZlYWU32AIF4AIB&aid=304142&lang=en-us&sb=1&src_elem=sb&src=searchresults&dest_id=-290692&dest_type=city&group_adults=2&no_rooms=1&group_children=0',
+    'https://www.booking.com/searchresults.html?ss=Ain+Sokhna%2C+Egypt&ssne=Hurghada&ssne_untouched=Hurghada&highlighted_hotels=788831&label=gen173nr-1FCAEoggI46AdIM1gEaEOIAQGYATG4ARfIAQ_YAQHoAQH4AQKIAgGoAgO4AtzRkroGwAIB0gIkNzI2MWQ0OTEtNzExNS00MTA4LTk0N2MtYjkwMDZmMmZlYWU32AIF4AIB&aid=304142&lang=en-us&sb=1&src_elem=sb&src=searchresults&dest_id=900040497&dest_type=city&checkin=2024-11-28&checkout=2024-11-29&group_adults=2&no_rooms=1&group_children=0',
+    'https://www.booking.com/searchresults.html?ss=Hurghada%2C+Egypt&ssne=Alexandria&ssne_untouched=Alexandria&highlighted_hotels=788831&efdco=1&label=gen173nr-1FCAEoggI46AdIM1gEaEOIAQGYATG4ARfIAQ_YAQHoAQH4AQKIAgGoAgO4AtzRkroGwAIB0gIkNzI2MWQ0OTEtNzExNS00MTA4LTk0N2MtYjkwMDZmMmZlYWU32AIF4AIB&aid=304142&lang=en-us&sb=1&src_elem=sb&src=hotel&dest_id=-290029&dest_type=city&group_adults=2&no_rooms=1&group_children=0'
+            ]
+#hotel_test=['https://www.booking.com/hotel/eg/the-grand-resort-hurghada.html?label=gen173nr-1FCAEoggI46AdIM1gEaEOIAQGYATG4ARfIAQ_YAQHoAQH4AQKIAgGoAgO4AtzRkroGwAIB0gIkNzI2MWQ0OTEtNzExNS00MTA4LTk0N2MtYjkwMDZmMmZlYWU32AIF4AIB&aid=304142&ucfs=1&arphpl=1&checkin=2024-11-26&checkout=2024-11-29&dest_id=-290029&dest_type=city&group_adults=2&req_adults=2&no_rooms=1&group_children=0&req_children=0&hpos=1&hapos=1&sr_order=popularity&srpvid=229b7882e20809ff&srepoch=1732556497&all_sr_blocks=71603801_376883182_2_85_0&highlighted_blocks=71603801_376883182_2_85_0&matching_block_id=71603801_376883182_2_85_0&sr_pri_blocks=71603801_376883182_2_85_0__21583&from_sustainable_property_sr=1&from=searchresults#tab-reviews','https://www.booking.com/hotel/eg/shellghada-blue-beach.html?label=gen173nr-1FCAEoggI46AdIM1gEaEOIAQGYATG4ARfIAQ_YAQHoAQH4AQKIAgGoAgO4AtzRkroGwAIB0gIkNzI2MWQ0OTEtNzExNS00MTA4LTk0N2MtYjkwMDZmMmZlYWU32AIF4AIB&aid=304142&ucfs=1&arphpl=1&dest_id=-290029&dest_type=city&group_adults=2&req_adults=2&no_rooms=1&group_children=0&req_children=0&hpos=2&hapos=2&sr_order=popularity&srpvid=6f6c7b5019bd0087&srepoch=1732555938&from=searchresults']
 hotel_links=[]
+
+
+# Function to make safe requests with retries
+MAX_RETRIES = 3
+def safe_request(url, headers):
+    global total_requests
+    for attempt in range(MAX_RETRIES):
+        try:
+            response = requests.get(url, headers=headers, timeout=10)
+            total_requests += 1  # Increment request count
+            response.raise_for_status()
+            return response
+        except RequestException as e:
+            print(f"Attempt {attempt + 1} failed for {url}: {e}")
+            sleep(2)  # Wait before retrying
+    return None  # Return None after MAX_RETRIES
 
 
 def get_hotels_link_in_each_city(cities, Cities_url):
     print("Scraping hotels links in each city started")
+    global total_requests
     for url, city in zip(Cities_url, cities):
         try:
             headers = random.choice(HEADERS_LIST)
-            response = requests.get(url, headers=headers)
+            response = safe_request(url, headers)
+
 
             print(city,"Connection success:",response.status_code)
             soup = bs(response.content, 'html.parser')
@@ -49,43 +91,58 @@ def get_hotels_link_in_each_city(cities, Cities_url):
             print("No hotels found")
         else:
             print("Hotels links scraped successfully")
+
+    print(f"Total HTTP requests made: {total_requests}")
     return hotel_links
 
 
 def get_reviews(hotel_links):
+    global selenium_requests
     print("Scraping reviews started")
-   # Initialize data containers
-    reviewer_name, review_title, review_good_text, review_bad_text, hotel_name = [], [], [], [], []
+
+    # Define the output CSV file
+    output_file = "hotel_reviews.csv"
+
+    # Initialize a flag to track whether headers are written
+    headers_written = False
 
     for hotel in hotel_links:
         hotelName = hotel['name']
         hotelLink = hotel['link']
-        print("Scraping reviews for",hotelName,"started")
+        print("Scraping reviews for", hotelName, "started")
+        selenium_requests += 1  # Increment Selenium request count
+
+        # Initialize data containers for the current hotel
+        reviewer_name, review_title, review_good_text, review_bad_text, city_name = [], [], [], [], []
+
         try:
             # Set up the Selenium driver (Firefox)
             driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()))
-            # Open hotel page
             driver.get(hotelLink)
             sleep(10)
-            # Wait for the reviews button to load and click it
-            #wait = WebDriverWait(driver, 20)
-            reviews_button = driver.find_element(By.XPATH, '//button[@data-testid="fr-read-all-reviews"]')
-            #reviews_button = wait.until(EC.element_to_be_clickable((By.XPATH, '//button[@data-testid="fr-read-all-reviews"]')))
-            reviews_button.click()
-            sleep(10)
-            # Wait for review cards to load
-            #wait.until(EC.presence_of_all_elements_located((By.XPATH, '//div[@data-testid="review-card"]')))
+
+            # Click the "Read All Reviews" button
+            try:
+                reviews_button = driver.find_element(By.XPATH, '//button[@data-testid="fr-read-all-reviews"]')
+                reviews_button.click()
+                sleep(10)
+            except Exception as e:
+                print(f"Error finding or clicking review button for {hotelName}: {e}")
+
             # Extract review cards
             reviews_cards = driver.find_elements(By.XPATH, '//div[@data-testid="review-card"]')
             
             for card in reviews_cards:
-                # Extract hotel name
-                hotel_name.append(hotelName)
 
+                #Extract city name
+                try:
+                    city_ = card.find_element(By.XPATH, '//a[@class="bui_breadcrumb__link_masked"]').text
+                except:
+                    city_ = "No City"
+                city_name.append(city_)
                 # Extract reviewer name
                 try:
-                    name = card.find_element(By.XPATH, './/div[@aria-label="Reviewer"]//div[@data-testid="review-avatar"] //div[@class="a3332d346a e6208ee469"]').text
-                    print(name)
+                    name = card.find_element(By.XPATH, './/div[@aria-label="Reviewer"]//div[@class="a3332d346a e6208ee469"]').text
                 except:
                     name = "No Name"
                 reviewer_name.append(name)
@@ -93,7 +150,6 @@ def get_reviews(hotel_links):
                 # Extract review title
                 try:
                     title = card.find_element(By.XPATH, './/div[@aria-label="Review"]//h3[@data-testid="review-title"]').text
-                    print(title)
                 except:
                     title = "No Title"
                 review_title.append(title)
@@ -101,7 +157,6 @@ def get_reviews(hotel_links):
                 # Extract positive review text
                 try:
                     good_text = card.find_element(By.XPATH, './/div[@aria-label="Review"]//div[@class="a53cbfa6de b5726afd0b"]').text
-                    print(good_text)
                 except:
                     good_text = "No Positive Review"
                 review_good_text.append(good_text)
@@ -109,21 +164,31 @@ def get_reviews(hotel_links):
                 # Extract negative review text
                 try:
                     bad_text = card.find_element(By.XPATH, './/div[@aria-label="Review"]//div[@data-testid="review-negative-text"]//div[@class="a53cbfa6de b5726afd0b"]').text
-                    print(bad_text)
                 except:
                     bad_text = "No Negative Review"
                 review_bad_text.append(bad_text)
-            
-            
-        
+
+            # Save the current hotel's reviews to the CSV file
+            if reviewer_name:  # Check if any reviews were scraped
+                reviews_df = pd.DataFrame({
+                    "City" : city_name,
+                    "Reviewer Name": reviewer_name,
+                    "Review Title": review_title,
+                    "Positive Review": review_good_text,
+                    "Negative Review": review_bad_text
+                })
+
+                # Append reviews to CSV
+                reviews_df.to_csv(output_file, mode='a', index=False, header=not headers_written)
+                headers_written = True  # Set flag to skip headers for subsequent hotels
+                print(f"Appended reviews for {hotelName} to {output_file}")
+
         except Exception as e:
-            #print(f"Error processing hotel {hotel}: {e}")
-            print(f"Error processing hotel {hotel_name} : {e}")
+            print(f"Error processing hotel {hotelName}: {e}")
+        finally:
+            driver.quit()
 
-
-        # Quit driver
-        driver.quit()
-
+    print(f"Total Selenium requests made: {selenium_requests}")
 
 
     if(reviewer_name == []):
@@ -146,5 +211,5 @@ if __name__ == "__main__":
     hotel_links = get_hotels_link_in_each_city(cities, Cities_url)
     get_reviews(hotel_links)
 
-    #last update the script can scrap reviews 
-    #   of the first hotel  
+    print(f"Final total requests: HTTP - {total_requests}, Selenium - {selenium_requests}")
+
